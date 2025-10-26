f03206fb96f5bb3f30e22dc728d8d2e7cacabb3a
ikernambo <ikernamboproject@gmail.com>
2025-10-26 05:25:16 -0600

    improved css in buttons and navbar

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -6,12 +6,12 @@ type contentProp = {
 
 export function Btn({content}: contentProp){
     return(
-        <button className="bg-[#0045b4] rounded-md p-2 m-3 w-40 text-white hover:bg-[#0051d3]"> {content} </button>
+        <button className="bg-[#0045b4] rounded-md p-2 m-3 w-40 text-white hover:bg-[#0051d3] transition duration-150 ease hover:scale-102 hover:shadow-md"> {content} </button>
     );
 }
 export function Btnnav({content}: contentProp){
     return(
-        <button className="bg-blue-900 rounded-md w-20 h-9 text-white border-white border-2 flex justify-center items-center"> {content} </button>
+        <button className="bg-blue-900 transition duration-150 ease rounded-md w-20 h-9 text-white border-white border-2 flex justify-center items-center"> {content} </button>
     );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         <Navbar/>
       <div className="flex flex-col self-center justify-center items-center h-150">
         <h1 className="text-white font-extrabold text-4xl">Welcome to Game Store!</h1>
+        <p className="text-white">Made by Iker Nambo</p>
         <div>
           <Link href='/store'><Btn content={'store'}/></Link>
           <Link href='/cart'><Btn content={'cart'}/></Link>
